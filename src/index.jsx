fbd89d698d4f300c24b629a2e4fdb1528d5e165e
Rosadev Solutions by Romulo Rosa <rosadevsolutions@gmail.com>
2024-11-19 19:35:09 -0300

    index.js para .jsx e com component Button

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Button from "./components/Button/Button";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Button />
   </React.StrictMode>
 );
