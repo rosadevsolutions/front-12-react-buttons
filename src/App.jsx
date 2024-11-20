6eaf737a872f28d99dc5d382de6562f4007c70cc
Romulo Rosa <romulorosadev@gmail.com>
2024-11-20 15:24:12 -0300

    feat: app with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import React from "react"
+import Button from "./components/Button/Button"
+
 const App = () => {
   return(
     <div className="App">
-      <h1>React + Sass</h1>
+      <Button
+        id="button-add"
+        className="button-add"
+        callToAction="Adicionar"
+      />
+
+      <Button
+        id="button-remove"
+        className="button-remove"
+        callToAction="Remover"
+      />
+
+      <Button
+        id="button-clean"
+        className="button-clean"
+        callToAction="Limpar"
+      />
     </div>
   )
 }
