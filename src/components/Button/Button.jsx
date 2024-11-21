f5071c8dc3ba8b7b0b6e4dc73da873deed247624
Romulo Rosa <romulorosadev@gmail.com>
2024-11-21 03:59:21 -0300

    feat: buttons props handleclicks

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,25 @@
-import "./Button.css"
+import "./Button.css";
 
-const Button = ({text, className, type, background}) => {
+const Button = ({disabled, styleType, text, type, onClick}) => {
+
+  const handleClickDefault = () => console.log("Clicou em Default")
+  const handleClickAdd     = () => console.log("Clicou em Adicionar")
+  const handleClickClear   = () => console.log("Clicou em Limpar")
+  const handleClickRemove  = () => console.log("Clicou em Remover")
+  const handleClickSend    = () => console.log("Clicou em Enviar")
+
   return(
     <button
+      className = {styleType ? `button ${styleType}` : "button"}
+      disabled = { disabled ? disabled : true }
+      onClick = {
+        onClick === "add" ? handleClickAdd
+        : onClick === "remove" ? handleClickRemove
+        :  onClick === "clear" ? handleClickClear
+        : onClick === "send" ? handleClickSend
+        : handleClickDefault
+      }
       type = {type ? type : "button"}
-      className = {className ? `button ${className}` : "button"}
     >
       <span>{text}</span>
     </button>
