ea02493f26a70c26e44a08410bfc672eba01607c
Romulo Rosa <romulorosadev@gmail.com>
2024-11-21 03:37:54 -0300

    feat: app com buttons  and handleClicks

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,42 @@ import Button from "./components/Button/Button"
 import "./index.css"
 
 const App = () => {
+  const handleClickAdd     = () => console.log("Clicou em Adicionar")
+  const handleClickClear   = () => console.log("Clicou em Limpar")
+  const handleClickDefault = () => console.log("Clicou em Default")
+  const handleClickRemove  = () => console.log("Clicou em Remover")
+  const handleClickSend    = () => console.log("Clicou em Enviar")
+
   return(
     <div className="app-wrapper">
       <Button
-        className="button-add"
+        onClick={handleClickSend}
+        styleType="button-send"
+        text="Enviar"
+        type="submit"
+      />
+
+      <Button
+        onClick={handleClickAdd}
+        styleType="button-add"
         text="Adicionar"
       />
 
       <Button
-        className="button-remove"
+        disabled="true"
+        onClick={handleClickRemove}
+        styleType="button-remove"
         text="Remover"
       />
 
       <Button
-        className="button-clear"
+        onClick={handleClickClear}
+        styleType="button-clear"
         text="Limpar"
       />
 
       <Button
+        onClick={handleClickDefault}
         text="Default"
       />
     </div>
