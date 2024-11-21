50e433802eb10eb0c3ece0de81b61dfb7f1f84fb
Romulo Rosa <romulorosadev@gmail.com>
2024-11-21 03:58:12 -0300

    fix: app js buttons disabled

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,38 @@ import Button from "./components/Button/Button"
 import "./index.css"
 
 const App = () => {
-  const handleClickAdd     = () => console.log("Clicou em Adicionar")
-  const handleClickClear   = () => console.log("Clicou em Limpar")
-  const handleClickDefault = () => console.log("Clicou em Default")
-  const handleClickRemove  = () => console.log("Clicou em Remover")
-  const handleClickSend    = () => console.log("Clicou em Enviar")
-
   return(
     <div className="app-wrapper">
       <Button
-        onClick={handleClickSend}
+        disabled={false}
+        onClick="send"
         styleType="button-send"
         text="Enviar"
         type="submit"
       />
 
       <Button
-        onClick={handleClickAdd}
+        disabled={false}
+        onClick="add"
         styleType="button-add"
         text="Adicionar"
       />
 
       <Button
-        disabled="true"
-        onClick={handleClickRemove}
+        disabled={false}
+        onClick="remove"
         styleType="button-remove"
         text="Remover"
       />
 
       <Button
-        onClick={handleClickClear}
+        disabled={false}
+        onClick="clear"
         styleType="button-clear"
         text="Limpar"
       />
 
       <Button
-        onClick={handleClickDefault}
         text="Default"
       />
     </div>
